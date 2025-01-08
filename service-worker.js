08a2ff137e8e86970202983bb4d9c5eed9ee1cdd
plafound <plafound.omah@gmail.com>
2025-01-08 21:24:32 +0700

    add PWA and fix some bug

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "igfollower-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./app.js",
+  "./instagram-192x192.png",
+  "./instagram-512x512.png"
+];
+
+// Instal Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ambil File dari Cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Aktivasi Service Worker
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
